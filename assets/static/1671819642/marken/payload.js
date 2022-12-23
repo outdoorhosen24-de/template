e10b9d0f213aa0c5ba29bccdaa10478a85672895
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-12-23 18:21:05 +0000

    deploy: 1bd836636878ab8828ccbff11722a98690c7529b

---
+++ assets/static/1671819642/marken/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/marken", {data:[{_img:{"/_ipx/f_webp,q_90,s_58x58/assets/images/images/mydeco-schiebevorhang-cool-grau-60-cm-x-300-cm-mydeco-5391180-1.jpg":"\u002Fassets\u002Fimages\u002Fmydeco-schiebevorhang-cool-grau-60-cm-x-300-cm-mydeco-5391180-1.webp"}}],fetch:{},mutations:void 0});
